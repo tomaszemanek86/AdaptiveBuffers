11aeb763b5e137f3dd76e915ef98c970c50f7d7f
Tomas Zemanek <tomas.zemanek@abbas.cz>
2024-02-02 18:50:41 +0100

    test updated

--- src/generator/cpp/test/cpp_tests/struct_with_size_arithmetics.cpp
+++ src/generator/cpp/test/cpp_tests/struct_with_size_arithmetics.cpp
@@ -10,11 +10,19 @@ UTEST(struct_with_size_arithmetics, serde) {
     ABSizeSer absize_ser;
     absize_ser.with_a(5);
     absize_ser.with_b(6);
+    absize_ser.with_c().get(0).set_data(180);
+    absize_ser.with_c().get(1).set_data(170);
+    absize_ser.with_c().get(2).set_data(160);
+    absize_ser.with_c().get(3).set_data(150);
     auto size = absize_ser.serialize(buffer);
-    ASSERT_EQ(size, 4);
+    ASSERT_EQ(size, 20);
 
     ABSizeDe absize_de(buffer);
     ASSERT_EQ(absize_de.a(), 5);
     ASSERT_EQ(absize_de.b(), 6);
-    ASSERT_EQ(absize_de.size(), 3);
+    ASSERT_EQ(absize_de.c().get(0).get_data(), 180);
+    ASSERT_EQ(absize_de.c().get(1).get_data(), 170);
+    ASSERT_EQ(absize_de.c().get(2).get_data(), 160);
+    ASSERT_EQ(absize_de.c().get(3).get_data(), 150);
+    ASSERT_EQ(absize_de.size(), 17);
 }
